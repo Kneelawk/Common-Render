15d482648d7f882a1a052b95544cc36596b9d3ce
Cyan Kneelawk <kneelawk@gmail.com>
2024-11-13 15:23:14 -0800

    Add some simple quad transforms

---
+++ modules/engine-api-xplat/src/main/java/com/kneelawk/krender/engine/api/util/transform/MaterialQuadTransform.java
@@ -0,0 +1,29 @@
+package com.kneelawk.krender.engine.api.util.transform;
+
+import org.jetbrains.annotations.UnknownNullability;
+
+import com.kneelawk.krender.engine.api.buffer.QuadEmitter;
+import com.kneelawk.krender.engine.api.buffer.QuadTransform;
+import com.kneelawk.krender.engine.api.buffer.QuadView;
+import com.kneelawk.krender.engine.api.material.RenderMaterial;
+
+/**
+ * Quad transform that sets the material of everything passed through.
+ */
+public class MaterialQuadTransform implements QuadTransform<RenderMaterial> {
+    private static final MaterialQuadTransform INSTANCE = new MaterialQuadTransform();
+
+    /**
+     * {@return the quad transform instance}
+     */
+    public static MaterialQuadTransform getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void transform(@UnknownNullability RenderMaterial context, QuadView input, QuadEmitter output) {
+        input.copyTo(output);
+        output.setMaterial(context);
+        output.emit();
+    }
+}
---
+++ modules/engine-api-xplat/src/main/java/com/kneelawk/krender/engine/api/util/transform/RetextureQuadTransform.java
@@ -0,0 +1,53 @@
+package com.kneelawk.krender.engine.api.util.transform;
+
+import org.jetbrains.annotations.UnknownNullability;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+
+import com.kneelawk.krender.engine.api.buffer.QuadEmitter;
+import com.kneelawk.krender.engine.api.buffer.QuadTransform;
+import com.kneelawk.krender.engine.api.buffer.QuadView;
+import com.kneelawk.krender.engine.api.texture.SpriteFinder;
+
+/**
+ * Quad transform that replaces the texture on all quads with the one specified.
+ * <p>
+ * Note: this assumes all textures are in the block atlas.
+ */
+public class RetextureQuadTransform implements QuadTransform<TextureAtlasSprite> {
+    private static final RetextureQuadTransform INSTANCE = new RetextureQuadTransform();
+
+    /**
+     * {@return the quad transform instance}
+     */
+    public static RetextureQuadTransform getInstance() {
+        return INSTANCE;
+    }
+
+    private final SpriteFinder finder =
+        SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlas.LOCATION_BLOCKS));
+
+    @Override
+    public void transform(@UnknownNullability TextureAtlasSprite context, QuadView input, QuadEmitter output) {
+        input.copyTo(output);
+
+        TextureAtlasSprite oldSprite = finder.find(input);
+        if (!oldSprite.contents().name().equals(MissingTextureAtlasSprite.getLocation())) {
+            for (int i = 0; i < 4; i++) {
+                float u = input.getU(i);
+                float v = input.getV(i);
+
+                u = oldSprite.getUOffset(u);
+                v = oldSprite.getVOffset(v);
+
+                u = context.getU(u);
+                v = context.getV(v);
+
+                output.setUv(i, u, v);
+            }
+        }
+    }
+}
